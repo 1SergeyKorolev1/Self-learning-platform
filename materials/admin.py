52f47bd9ed01be2d64243fc167290ac2034bfb83
Sergey Korolev <bongrichard@rambler.ru>
2024-08-02 00:09:47 +0300

    Добавлены модель курса, урлы, контроллер и серилизаторы на CRUD для курса. Добавлен вывод курсов в админку

--- materials/admin.py
+++ materials/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from materials.models import Course
+
+@admin.register(Course)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("pk", "name")
--- materials/views.py
+++ materials/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet, generics
+from materials.models import Course
+from materials.serializers import CourseDitailSerializer, CourseSerializer
 
-# Create your views here.
+
+class CourseViewSet(ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return CourseDitailSerializer
+        return CourseSerializer
+
+    def perform_create(self, serializer):
+        course = serializer.save()
+        course.owner = self.request.user
+        course.save()
+
+
