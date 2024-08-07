78176ef4d3088195230038fea58a5dd9026a3c9f
Sergey Korolev <bongrichard@rambler.ru>
2024-08-07 11:51:33 +0300

    Команды csu и csu_none изменены на cpu (CreatePackUsers) - добавлены три пользователя. Добавлены фикстуры на группы, пользователей и материалы...

---
+++ users/management/commands/cpu_none.py
@@ -0,0 +1,23 @@
+from django.core.management import BaseCommand
+
+from users.models import User
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        user = User.objects.get(
+            email="admin@rambler.ru",
+        ).delete()
+
+        user = User.objects.get(
+            email="alexandr@top.ru",
+        ).delete()
+
+        user = User.objects.get(
+            email="sergo@top.ru",
+        ).delete()
+
+        user = User.objects.get(
+            email="test@top.ru",
+        ).delete()
